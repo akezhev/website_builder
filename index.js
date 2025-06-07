a8bff54d5a3834d5130d1f8232e9b53ea9a6d0ad
akezhev <akezh@mail.ru>
2025-06-07 23:09:28 +0300

    changed some file, start project

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const model = [];
+const model = [
+  {type: 'title', value: 'hello world from JS'},
+  {type: 'text', value: 'here we go some text'},
+  {type: 'columns', value: [
+    '11111111111111',
+    '22222222222222',
+    '33333333333333',
+  ]}
+];
+
+const $site = document.querySelector('#site');
+
+model.forEach(block => {
+  let html = '';
+
+  if (block.type === 'title') {
+    html = `
+      <div class="row">
+        <div class="col-sm">
+          <h1>${block.value}</h1>
+        </div>
+      </div>
+    `
+  } else if (block.type === 'text') {
+    html = `
+      <div class="row">
+        <div class="col-sm">
+          <p>${block.value}</p>
+        </div>
+      </div>
+    `
+  } else if (block.type === 'columns') {
+
+  }
+
+  $site.insertAdjacentHTML('beforeend', html)
+  
+});
