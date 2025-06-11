72d2d6283cc335bf0a392ac5465c2dfacce3d056
akezhev <akezh@mail.ru>
2025-06-11 16:57:41 +0300

    added some file, added parcel build

---
+++ src/model.js
@@ -0,0 +1,11 @@
+export const model = [
+  {type: 'title', value: 'Website builder from JS'},
+  {type: 'text', value: 'here we go some text'},
+  {type: 'columns', value: [
+    '11111111111111',
+    '22222222222222',
+    '33333333333333',
+    '44444444444444 '
+  ]},
+  {type: 'image', value: './src/assets/image.png'}
+];
