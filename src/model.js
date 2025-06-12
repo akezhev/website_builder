a23280e0171bc2f44bfd3c892f440401d825115d
akezhev <akezh@mail.ru>
2025-06-12 21:29:04 +0300

    added style-properties from model.js

--- src/model.js
+++ src/model.js
@@ -1,16 +1,44 @@
-import image from './assets/image.png'
+import image from './assets/image.png';
+
+const text = `Если нужен сайт на JavaScript, оставь заявку: <a href="https://rutube.com" target="_blank">Tim Akezh</a>`
 
 export const model = [
   {type: 'title', value: 'Website builder from JavaScript', options: {
     tag: 'h2',
-    styles: `background: linear-gradient(#751FB2, #3B6AB2); color: #fff; text-align: center; padding: 1.5rem;`,
+    styles: {
+      background: 'linear-gradient(#751FB2, #3B6AB2)',
+      color: '#fff',
+      padding: '1.5rem',
+      'text-align': 'center',
+    }
   }},
-  {type: 'text', value: 'here we go some text'},
+
+  {type: 'text', value: text, options: {
+    styles: {
+      background: 'linear-gradient(#751FB2, #3B6AB2)',
+      padding: '1rem',
+      'font-weight': 'bold'
+    }
+  }},
+
+  {type: 'image', value: image, options: {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    }
+  }},
+
   {type: 'columns', value: [
-    '11111111111111',
-    '22222222222222',
-    '33333333333333',
-    '44444444444444 '
-  ]},
-  {type: 'image', value: image,}
+    'Приложение на чистом JavaScript, без использования библиотек',
+    'Узнаешь как работают принцыпы SOLID и ООП в JavaScript',
+    'JavaScript - это просто, и интересно. Научись создавать любые UI своими руками',
+  ], options: {
+    styles: {
+      background: 'linear-gradient(#751FB2, #3B6AB2)',
+      padding: '1rem',
+      'font-weight': 'bold'
+    }
+  }},
+
 ];
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,22 @@
-export function row(content, styles='') {
+export function row(content, styles = '') {
   return `<div class="row" style="${styles}">${content}</div>`
 }
 
 export function col(content) {
   return `<div class="col-sm">${content}</div>`
+}
+
+// export function css(styles={}) {
+//   const toString = key => `${key}: ${styles[key]}`
+//   return Object.keys(styles).map(toString).join(';')
+// }
+
+// Деструктуризацию в аргументах функции map для лучшей читаемости
+export function css(styles = {}) {
+  if (typeof styles !== 'object' || styles === null) {
+    return '';
+  }
+  return Object.entries(styles)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join(';');
 }
