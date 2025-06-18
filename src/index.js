720ac17e7b520649069ffe24b77896903a24aaf9
akezhev <akezh@mail.ru>
2025-06-18 11:31:09 +0300

    expanded functionality by adding a sidebar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import {model} from './model';
 import {Site} from './classes/site';
+import { Sidebar } from './classes/sidebar';
 import './styles/main.css';
 
 const site = new Site('#site');
 
-site.render(model)
+site.render(model);
 
 
+const sidebar = new Sidebar('#panel');
