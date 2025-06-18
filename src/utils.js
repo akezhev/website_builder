dc5d19c251973d12b3c4c9ab8e1c1e5d4c56673b
akezhev <akezh@mail.ru>
2025-06-18 11:51:54 +0300

    made some changes to the utils.js file

--- src/utils.js
+++ src/utils.js
@@ -25,10 +25,10 @@ export function block(type) {
   return `
     <form name="${type}">
       <h5>${type}</h5>
-      <div class="form-group">
+      <div class="form-group mb-3">
         <input class="form-control form-control-sm" name="value" placeholder="value">
       </div>
-      <div class="form-group">
+      <div class="form-group mb-3">
         <input class="form-control form-control-sm" name="styles" placeholder="styles">
       </div>
       <button type="submit" class="btn btn-primary btn-sm">Добавить</button>
