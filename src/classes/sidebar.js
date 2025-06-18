b7a6158293888f4e98b8b76e911607cbb5a6ba53
akezhev <akezh@mail.ru>
2025-06-18 11:48:57 +0300

    made some changes to the sidebar file

--- src/classes/sidebar.js
+++ src/classes/sidebar.js
@@ -1,4 +1,5 @@
 import {block} from '../utils';
+import { TextBlock, TitleBlock } from './blocks';
 
 export class Sidebar {
   constructor(selector) {
@@ -9,6 +10,7 @@ export class Sidebar {
 
   init() {
     this.$el.insertAdjacentHTML('afterbegin', this.template);
+    this.$el.addEventListener('submit', this.add)
   }
 
   get template() {
@@ -16,6 +18,19 @@ export class Sidebar {
       block('text'),
       block('title')
     ].join('')
+  }
+
+  add(event) {
+    event.preventDefault();
+
+    const type = event.target.name;
+    const value = event.target.value;
+    const styles = event.targer.styles;
+
+    const newBlock = type === 'text'
+      ? new TextBlock(value, {styles})
+      : new TitleBlock(value, {styles})
+
   }
 }
 
